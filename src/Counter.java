84fa70cb4fdd401377e553accc6832f3f2e8f6a7
Jakub Kudzia <jakub.kudzia@outlook.com>
2015-12-02 23:00:29 +0100

    add Counter class

---
+++ src/Counter.java
@@ -0,0 +1,13 @@
+public class Counter {
+
+    private int counter;
+
+    Counter(){this.counter = 0;}
+
+    public synchronized int getCounter() {
+        return counter;
+    }
+
+    synchronized void increase(){ this.counter++; }
+
+}
